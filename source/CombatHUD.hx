215de89ea61d8f82c88c6bae50187e126d066dce
Will Kuntze <will.kuntze@gmail.com>
2025-11-26 15:16:31 -0500

    added imports for combat HUD

---
+++ source/CombatHUD.hx
@@ -0,0 +1,18 @@
+package;
+
+import flash.filters.ColorMatrixFilter;
+import flash.geom.Matrix;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.effects.chainable.FlxEffectSprite;
+import flixel.addons.effects.chainable.FlxWaveEffect;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.sound.FlxSound;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.ui.FlxBar;
+import flixel.util.FlxColor;
+
+using flixel.util.FlxSpriteUtil;
+
